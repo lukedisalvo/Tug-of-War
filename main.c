c4be42d73d326f1d6b6bb895684872b544715b11
luked <luked@DESKTOP-J0U6EBT>
2020-02-27 12:59:19 -0500

    Final, working solution with comments

--- main.c
+++ main.c
@@ -30,25 +30,61 @@ Graphics_Context InitGraphics()
     return context;
 }
 
+//start of the initialization of projects
 // According to the schematics on page 37 of the Launchpad user guide,
-// left button (button 1) is connected to pin 1 (bit1)
-#define LEFT_BUTTON BIT1
+//define what is needed for LED1
+#define PRESSED 0 // When a button is pressed, it is grounded (logic 0)
+#define LEFT_BUTTON BIT1 // left button of launchpad (button 1) is connected to pin 1 (bit1)
+#define LEFT_LED BIT0 // left LED (LED 1) is connected to pin 0 (bit0)
 
-// According to the schematics on page 37 of the Launchpad user guide,
-// left LED (LED 1) is connected to pin 0 (bit0)
-#define LEFT_LED BIT0
+//define what is needed for LED2
+#define RIGHT_BUTTON BIT4 // right button of launchpad (button 2) is connected to pin 1 (bit4)
+#define RIGHT_LED_RED BIT0 // LED2s red component is connected to bit 0
+#define RIGHT_LED_GREEN BIT1 // LED2s green component is connected to bit 1
+#define RIGHT_LED_BLUE BIT2 // LED2s blue component is connected to bit 2
+#define SETTER 0xF8
 
-// According to the schematics on page 37 of the Launchpad user guide,
-// When a button is pressed, it is grounded (logic 0)
-#define PRESSED 0
+#define BUTTON_S1 BIT1 // top button of boosterpack (button 1) is connected to (bit1)
+#define BUTTON_S2 BIT5 // bottom button of boosterpack (button 1) is connected to (bit1)
+#define BOOSTER_REDLED BIT6 // BLEDs red component is connected to bit 6
+#define SETTER_BOOSTER 0xAF //clears the LED components
+#define BOOSTER_LED_RED BIT6 //BLEDs red component is connected to bit 6
+#define BOOSTER_LED_GREEN BIT4 //BLEDs green component is connected to bit 4
+#define BOOSTER_LED_BLUE BIT6 //BLEDs blue component is connected to bit 6
 
-// This initializes all the peripherals
+//This initializes all the functions of the peripherals used, many have names that tell exactly what the
+//function does.
+//initializes all peripherals for LED1
 void init_Launchpad_LED1();
 void initialize();
 void TurnOn_Launchpad_LED1();
 void TurnOff_Launchpad_LED1();
 char SwitchStatus_Launchpad_Button1();
+//initializes all peripherals for LED2
+void init_Launchpad_LED2();
+void TurnOn_Launchpad_LED2_RED();
+void TurnOff_Launchpad_LED2_RED();
+void TurnOn_Launchpad_LED2_GREEN();
+void TurnOff_Launchpad_LED2_GREEN();
+void TurnOn_Launchpad_LED2_BLUE();
+void TurnOff_Launchpad_LED2_BLUE();
+char SwitchStatus_Launchpad_Button2();
+void init_Launchpad_Right_Button();
+void init_Launchpad_Left_Button();
+//initializes all peripherals for BLED
+void init_Boosterpack_LED();
+void TurnOn_Boosterpack_RED();
+void TurnOff_Boosterpack_RED();
+void TurnOn_Boosterpack_GREEN();
+void TurnOff_Boosterpack_GREEN();
+void TurnOn_Boosterpack_BLUE();
+void TurnOff_Boosterpack_BLUE();
+void init_Boosterpack_Top_Button();
+void init_Boosterpack_Bottom_Button();
+char SwitchStatus_Boosterpack_Button1();
+char SwitchStatus_Boosterpack_Button2();
 
+//start of the initialization of LED1 and LED2, as well as buttons left and right on the launchpad.
 void init_Launchpad_LED1()
 {
     // step 3.1: write a 0 to PxDIR for the specific bit you want
@@ -61,47 +97,24 @@ void init_Launchpad_LED1()
     P1OUT |= LEFT_BUTTON;  // select pull-up
 
 }
-
+//turns on LED1's red component by setting bit 0 to high
 void TurnOn_Launchpad_LED1()
 {
     //GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
     P1OUT = P1OUT |  LEFT_LED;
 }
-
+//turns off LED1's red component by setting bit 0 to low
 void TurnOff_Launchpad_LED1()
 {
     //GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
     P1OUT = P1OUT & ~LEFT_LED;
 }
-
+//returns the statis of button 1
 char SwitchStatus_Launchpad_Button1()
 {
     //return (GPIO_getInputPinValue(GPIO_PORT_P1, GPIO_PIN1));
     return P1IN & LEFT_BUTTON;
 }
-
-//start of initialization of RIGHT button and LED2
-
-// According to the schematics on page 37 of the Launchpad user guide,
-// Right button (button 2) is connected to pin 1 (bit4)
-#define RIGHT_BUTTON BIT4
-#define RIGHT_LED_RED BIT0
-#define RIGHT_LED_GREEN BIT1
-#define RIGHT_LED_BLUE BIT2
-#define SETTER 0xF8
-
-
-// This function initializes all the peripherals
-void init_Launchpad_LED2();
-void TurnOn_Launchpad_LED2_RED();
-void TurnOff_Launchpad_LED2_RED();
-void TurnOn_Launchpad_LED2_GREEN();
-void TurnOff_Launchpad_LED2_GREEN();
-void TurnOn_Launchpad_LED2_BLUE();
-void TurnOff_Launchpad_LED2_BLUE();
-char SwitchStatus_Launchpad_Button2();
-void init_Launchpad_Right_Button();
-void init_Launchpad_Left_Button();
 
 void init_Launchpad_Left_Button()
 {
@@ -139,43 +152,37 @@ void init_Launchpad_LED2()
     P2OUT |= SETTER;  // select pull-up
 
 }
-
+//turns on LED2's red component by setting bit 1 to high
 void TurnOn_Launchpad_LED2_RED()
 {
-    P2DIR = P2DIR & SETTER;
-    P2DIR = P2DIR | RIGHT_LED_RED;
     P2OUT = P2OUT | RIGHT_LED_RED;
-
 }
-
+//turns off LED2's red component by setting bit 1 to low
 void TurnOff_Launchpad_LED2_RED()
 {
     P2OUT = P2OUT & ~RIGHT_LED_RED;
 }
-
+//turns on LED2's green component by setting bit 1 to high
 void TurnOn_Launchpad_LED2_GREEN()
 {
-    P2DIR = P2DIR & SETTER;
-    P2DIR = P2DIR | RIGHT_LED_GREEN;
     P2OUT = P2OUT | RIGHT_LED_GREEN;
 }
-
+//turns off LED2's red component by setting bit 1 to low
 void TurnOff_Launchpad_LED2_GREEN()
 {
     P2OUT = P2OUT & ~RIGHT_LED_GREEN;
 }
-
+//turns on LED2's blue component by setting bit 2 to high
 void TurnOn_Launchpad_LED2_BLUE()
 {
-    P2DIR = P2DIR & SETTER;
-    P2DIR = P2DIR | RIGHT_LED_BLUE;
     P2OUT = P2OUT | RIGHT_LED_BLUE;
 }
-
+//turns off LED2's blue component by setting bit 2 to low
 void TurnOff_Launchpad_LED2_BLUE()
 {
     P2OUT = P2OUT & ~RIGHT_LED_BLUE;
 }
+//returns the status of the right button
 char SwitchStatus_Launchpad_Button2()
 {
 
@@ -183,31 +190,6 @@ char SwitchStatus_Launchpad_Button2()
 }
 
 //Start of the initialization for Buttons S1, S2 and LEDs on Booster pack
-
-// This initializes all the peripherals
-void init_Boosterpack_LED();
-void TurnOn_Boosterpack_RED();
-void TurnOff_Boosterpack_RED();
-void TurnOn_Boosterpack_GREEN();
-void TurnOff_Boosterpack_GREEN();
-void TurnOn_Boosterpack_BLUE();
-void TurnOff_Boosterpack_BLUE();
-void init_Boosterpack_Top_Button();
-void init_Boosterpack_Bottom_Button();
-void TurnOn_Boosterpack_Yellow();
-void TurnOff_Boosterpack_Yellow();
-char SwitchStatus_Boosterpack_Button1();
-char SwitchStatus_Boosterpack_Button2();
-
-//Define what is needed
-#define BUTTON_S1 BIT1
-#define BUTTON_S2 BIT5
-#define BOOSTER_REDLED BIT6
-#define SETTER_BOOSTER 0xAF
-#define BOOSTER_LED_RED BIT6
-#define BOOSTER_LED_GREEN BIT4
-#define BOOSTER_LED_BLUE BIT6
-
 
 void init_Boosterpack_Top_Button()
 {
@@ -245,60 +227,47 @@ void init_Boosterpack_LED()
     P2OUT |= SETTER_BOOSTER;
 
 }
-
+//turns on BLED's red component by setting bit 6 to high, note it is connected to pin 2
 void TurnOn_Boosterpack_RED()
 {
-    P2DIR = P2DIR & SETTER_BOOSTER;
-    P2DIR = P2DIR | BOOSTER_LED_RED;
     P2OUT = P2OUT | BOOSTER_LED_RED;
 }
+//turns off BLED's red component by setting bit 6 to low
 void TurnOff_Boosterpack_RED()
 {
     P2OUT = P2OUT & ~BOOSTER_LED_RED;
 }
+//turns on BLED's green component by setting bit 4 to high, note it is connected to pin 2
 void TurnOn_Boosterpack_GREEN()
 {
-    P2DIR = P2DIR & SETTER_BOOSTER;
-    P2DIR = P2DIR | BOOSTER_LED_GREEN;
     P2OUT = P2OUT | BOOSTER_LED_GREEN;
 }
+//turns off BLED's green component by setting bit 4 to low
 void TurnOff_Boosterpack_GREEN()
 {
     P2OUT = P2OUT & ~BOOSTER_LED_GREEN;
 }
+//turns on BLED's blue component by setting bit 6 to high, note it is connected to pin 5
 void TurnOn_Boosterpack_BLUE()
 {
-    P5DIR = P5DIR & BOOSTER_LED_BLUE;
-    P5DIR = P5DIR | BOOSTER_LED_BLUE;
     P5OUT = P5OUT | BOOSTER_LED_BLUE;
 }
+//turns off BLED's red component by setting bit 6 to high
 void TurnOff_Boosterpack_BLUE()
 {
     P5OUT = P5OUT & ~BOOSTER_LED_BLUE;
 }
-
+//returns the status of the top button
 char SwitchStatus_Boosterpack_Button1()
 {
     return P5IN & BUTTON_S1;
 }
+//returns the status of the bottom button
 char SwitchStatus_Boosterpack_Button2()
 {
     return P3IN & BUTTON_S2;
 }
-/*
-void TurnOn_Boosterpack_Yellow()
-{
-    P2OUT = P2OUT & ~BOOSTER_LED_RED;
-    P5DIR = P5DIR & BOOSTER_LED_BLUE;
-    P5DIR = P5DIR | BOOSTER_LED_BLUE;
-    P5OUT = P5OUT | BOOSTER_LED_BLUE;
-}
-void TurnOff_Boosterpack_Yellow()
-{
-    P2OUT = P2OUT & ~BOOSTER_LED_RED;
-    P2OUT = P2OUT & ~BOOSTER_LED_GREEN;
-}
-*/
+//Initializes all LED components and all 4 buttons
 void initialize()
 {
 
@@ -306,25 +275,25 @@ void initialize()
     // We do this at the beginning of all our programs for now.Later we learn more about it.
     WDT_A_hold(WDT_A_BASE);
 
-    // step 2: Initializing LED1
+
     // According to table 12.1 on page 678 of MSP432 User guide,
     // to create an output, all you need is to write a 1 to PxDIR for the specific bit you want
     // A common mistake is to write P1DIR = LEFT_LED instead of P1DIR |= LEFT_LED;
     P1DIR |= LEFT_LED;
     P2DIR |= SETTER;
-    P5DIR |= BOOSTER_REDLED;
     P2DIR |= SETTER_BOOSTER;
-
+    P2DIR |= RIGHT_LED_RED;
+    P2DIR |= RIGHT_LED_GREEN;
+    P2DIR |= RIGHT_LED_BLUE;
+    P5DIR |= BOOSTER_REDLED;
+    P2DIR |= BOOSTER_LED_GREEN;
+    P5DIR |= BOOSTER_LED_BLUE;
 
     init_Launchpad_Left_Button();
     init_Launchpad_Right_Button();
     init_Boosterpack_Top_Button();
     init_Boosterpack_Bottom_Button();
 }
-
-
-
-
 
 int main(void)
 {
@@ -346,9 +315,8 @@ int main(void)
         snprintf(buffer, BUFFER_SIZE, "%02d", right);
         Graphics_drawString(&context, (int8_t*) buffer, -1, 100, 100, true);
 
-        initialize();
-         //if launchpad right button is pressed, the right side will increase
-
+        initialize(); //called from above to initialize all LED and buttons
+        //if launchpad right button is pressed, the right side will increase and update the graphics
         if (SwitchStatus_Launchpad_Button2() != PRESSED)
             left = left;
         else if (left == 40)
@@ -357,14 +325,14 @@ int main(void)
             left++;
             snprintf(buffer, BUFFER_SIZE, "%02d", left);
             Graphics_drawString(&context, (int8_t*) buffer, -1, 20, 20, true);
-        //if launchpad left button is pressed, the right side will decrease
+        //if launchpad left button is pressed, the right side will decrease and update the graphics
         if (SwitchStatus_Launchpad_Button1() != PRESSED)
             left = left;
         else if (left == 0)
             left = left;
         else
             left--;
-        //if Boosterpack top button is pressed, the right side will decrease
+        //if Boosterpack top button is pressed, the right side will increase and update the graphics
         if (SwitchStatus_Boosterpack_Button1() != PRESSED)
             right = right;
         else if (right == 40)
@@ -373,7 +341,7 @@ int main(void)
             right++;
             snprintf(buffer, BUFFER_SIZE, "%02d", right);
             Graphics_drawString(&context, (int8_t*) buffer, -1, 100, 100, true);
-        //if Boosterpack bottom button is pressed, the right side will decrease
+        //if Boosterpack bottom button is pressed, the right side will decrease and update the graphics
         if (SwitchStatus_Boosterpack_Button2() != PRESSED)
             right = right;
         else if (right == 0)
@@ -383,10 +351,8 @@ int main(void)
             snprintf(buffer, BUFFER_SIZE, "%02d", right);
             Graphics_drawString(&context, (int8_t*) buffer, -1, 100, 100, true);
 
-
-
         //If the difference between left and right is within 10, then LED1 is on
-        if (left - right <= 10)
+        if (left - right <= 10 && right - left <= 10)
             TurnOn_Launchpad_LED1();
         else
             TurnOff_Launchpad_LED1();
